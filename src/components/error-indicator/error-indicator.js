54d6afcf06207c6bdc2b63a5d8d42b0690aee181
Nadya <nadya.zhidenko@bk.ru>
2021-01-29 13:46:14 +0300

    React Components added

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './error-indicator.css';
+
+
+const ErrorIndicator = () => {
+    return(
+        <div className='error-indicator'>
+            <span className='boom'> Boooom!</span>
+        </div>
+    )
+}
+
+export default ErrorIndicator;
