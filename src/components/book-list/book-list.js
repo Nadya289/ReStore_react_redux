c86c8107a86c6187a1c22b9abd632f2edb8869cd
Nadya <nadya.zhidenko@bk.ru>
2021-02-06 16:43:06 +0300

    Added elements (booksItem) to arr

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -6,18 +6,21 @@ import {bindActionCreators} from 'redux';
 import withBookstoreService, {withBookService} from '../hoc/with-bookstore-service';
 import Spinner from '../../components/spinner/spinner';
 import ErrorIndicator from '../../components/error-indicator/error-indicator';
-import { fetchBooks} from '../../actions/index';
+import { fetchBooks, bookAddedToCart} from '../../actions/index';
 import {compose}  from '../../utils/cpmpose';
 import './book-list.css';
 
-const BookList = ({books}) => {
+const BookList = ({books, onAddedToCart}) => {
     return(
         <ul className='book-list'>
             {
             books.map((book) => {
                 return(
                     <li key = {book.id}>
-                        <BookListItem book = {book}/>
+                        <BookListItem 
+                            book = {book}
+                            onAddedToCart = {() => onAddedToCart(book.id)}
+                        />
                     </li>
                 );
             })
@@ -34,14 +37,14 @@ class BookListContainer extends Component{
     }
 
     render (){
-        const { books, loading, error } = this.props;
+        const { books, loading, error, onAddedToCart } = this.props;
         if(loading){
             return <Spinner/>
         }
         if(error){
             return <ErrorIndicator />
         }
-    return <BookList books = {books}/>;
+    return <BookList books = {books} onAddedToCart = {onAddedToCart}/>;
      }
 }
 
@@ -52,7 +55,8 @@ const mapStateToProps = ({books,loading, error}) => {
 
 const mapDispatchToProps = (dispatch, {bookstoreService}) => {
     return{
-        fetchBooks: fetchBooks(bookstoreService, dispatch)
+        fetchBooks: fetchBooks(bookstoreService, dispatch),
+        onAddedToCart: (id) => dispatch(bookAddedToCart(id))
         
     };
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,25 +2,13 @@ const initialState = {
     books: [],
     loading:true,
     error:null,
-    cartItems: [
-        {
-            id:1,
-            title: 'book',
-            count:3,
-            total:150
-        },
-        {
-            id:1,
-            title: 'book',
-            count:3,
-            total:150
-        },
-
-    ],
+    cartItems: [],
     orderTotal: 88
 };
 
 const reducer = (state = initialState, action) => {
+
+    console.log(action.type);
     switch(action.type){
         case 'FETCH_BOOKS_REQUEST':
             return{
@@ -44,6 +32,24 @@ const reducer = (state = initialState, action) => {
             loading: false,
             error: action.payload
         };
+
+        case 'BOOK_ADDED_TO_CART':
+            const bookId = action.payload;
+            const book = state.books.find((book) => book.id == bookId);
+            const newItem = {
+                id: book.id,
+                title: book.title,
+                count: 1,
+                total: book.price
+            };
+            return{
+                ...state,
+                cartItems: [
+                    ...state.cartItems,
+                    newItem
+                ]
+            };
+
         default:
             return state;
     };
