94755cb1bc1b914f815ee696a4986cc8f5c82476
Nadya <nadya.zhidenko@bk.ru>
2021-02-06 14:29:47 +0300

    New redux component added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,37 @@
 const initialState = {
     books: [],
     loading:true,
-    error:null
+    error:null,
+    cartItems: [
+        {
+            id:1,
+            title: 'book',
+            count:3,
+            total:150
+        },
+        {
+            id:1,
+            title: 'book',
+            count:3,
+            total:150
+        },
+
+    ],
+    orderTotal: 88
 };
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
         case 'FETCH_BOOKS_REQUEST':
             return{
+                ...state,
                 books:[],
                 loading: true,
                 error: null
             };
         case 'FETCH_BOOKS_SUCCESS': 
         return{
+            ...state,
             books: action.payload,
             loading:false,
             error: null
@@ -21,6 +39,7 @@ const reducer = (state = initialState, action) => {
         
         case 'FETCH_BOOKS_FAILURE':
         return{
+            ...state,
             books: [],
             loading: false,
             error: action.payload
