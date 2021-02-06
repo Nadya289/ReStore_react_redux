b161a319f50ad4c0b0c26c736e716ecb281692e0
Nadya <nadya.zhidenko@bk.ru>
2021-02-06 12:45:22 +0300

    ownProps added

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -13,15 +13,13 @@ import './book-list.css';
 class BookList extends Component{
 
     componentDidMount(){
+        this.props.fetchBooks();
 
-        const {bookstoreService, booksLoaded, 
-                booksRequested, booksError} = this.props;
-        booksRequested();
-        bookstoreService.getBooks()
-            .then((data) => booksLoaded(data))
-            .catch((err) => booksError(err));
+
+        // const {bookstoreService, booksLoaded, 
+        //         booksRequested, booksError} = this.props;
+        // 
     }
-
 
     render (){
         const { books, loading, error } = this.props;
@@ -51,10 +49,16 @@ const mapStateToProps = ({books,loading, error}) => {
     return{books, loading, error};
 };
 
-const mapDispatchToProps = {
-    booksLoaded,
-    booksRequested,
-    booksError
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const{bookstoreService} = ownProps;
+    return{
+        fetchBooks: () => {
+           dispatch(booksRequested());
+            bookstoreService.getBooks()
+            .then((data) => dispatch(booksLoaded(data)))
+            .catch((err) => dispatch(booksError(err)));
+        }
+    }
 };
 
 export default compose(
